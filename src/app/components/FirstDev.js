d54321dd9c38da8e2ee26359a4ba60a3ef94f080
Aya Joudeh <ayajoudeh@icloud.com>
2023-09-11 12:08:17 +0300

    fixing sizing

--- src/app/components/FirstDev.js
+++ src/app/components/FirstDev.js
@@ -4,7 +4,7 @@ import Card from './Card'
 
 const FirstDev = () => {
     return ( <div>
-        <h1 className="text-center text-xl text-primary-800 font-bold mb-7">Next JS Test</h1>
+        <h1 className="text-center text-xxl text-primary-800 font-bold mb-10 text-3xl">Next JS Test</h1>
         <p className="text-center mb-40 " style={{ maxWidth: '70rem' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
               consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
--- src/app/components/SecondDev.js
+++ src/app/components/SecondDev.js
@@ -5,7 +5,7 @@ import CardData from './CardData.json';
 const SecondDev = () => {
   return (
     <div className='bg-greydark-50 p-10'>
-      <h1 className="text-center text-xxl text-black font-bold mb-10 text-4xl">Title Goes Here</h1>
+      <h1 className="text-center text-xxl text-black font-bold mb-10 text-3xl">Title Goes Here</h1>
       <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum in voluptate velit esse.</p>
 
       <div className="flex flex-wrap justify-center">
@@ -30,3 +30,4 @@ const SecondDev = () => {
 }
 
 export default SecondDev;
+
